5c6d3596c0dfab6802433dd6e6e46750526967e1
Saumya Nethmini <saumya123na@gmail.com>
2025-06-17 21:53:36 +0530

    V1.3
    frontend done

--- weather_app/src/components/Weather.jsx
+++ weather_app/src/components/Weather.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
-import cloud_icon from '../assets/cloud.png'
+import humidity_icon from '../assets/humidity.png'
+import wind_icon from '../assets/wind.png'
+{/*import cloud_icon from '../assets/cloud.png'
 import drizzle_icon from '../assets/drizzle.png'
-import humidit_icon from '../assets/humidit.png'
+
 import rain_icon from '../assets/rain.png'
 import snow_icon from '../assets/snow.png'
-import wind_icon from '../assets/wind.png'
+*/}
 
 const Weather = () => {
   return (
@@ -19,7 +21,26 @@ const Weather = () => {
         </div>
 
         {/* for weather details */}
-        <img src={} alt="" />
+        <img src={clear_icon} alt="" className='weather-icon'/>
+        <p className='temperature'>16° C</p>
+        <p className='location'>London</p>
+
+        <div className="weather-data">
+            <div className="col">
+                <img src={humidity_icon} alt="" />
+                <div>
+                    <p>91 %</p>
+                    <span>Humidity</span>
+                </div>
+            </div>
+            <div className="col">
+                <img src={wind_icon} alt="" />
+                <div>
+                    <p>3.6 Km/h</p>
+                    <span>Wind Speed</span>
+                </div>
+            </div>
+        </div>
 
     </div>
   )
